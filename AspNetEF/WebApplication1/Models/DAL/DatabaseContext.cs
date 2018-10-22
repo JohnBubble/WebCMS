6a9c094b212182c82dc809f00eacbb339c25bf48
JohnBubble <JohnBubble2@yandex.ru>
2018-10-22 23:07:30 +0300

    project added

---
+++ AspNetEF/WebApplication1/Models/DAL/DatabaseContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           
+            // base.OnModelCreating(modelBuilder);
+            //пусть будет users!
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); 
+
+        }
+    }
+
+    public class DataBaseInitializer : DropCreateDatabaseIfModelChanges<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            
+
+            context.Users.Add(
+                new User()
+                {
+                    Email = "test@mail.ru",
+                    Password = "1234"
+                }
+                );
+            context.SaveChanges();
+            //base.Seed(context);
+        }
+    }
+}
